f76d40c023942dcdbe0b38f196e85e644c0ae3e6
Murian <y2k001hj@gmail.com>
2026-03-23 10:24:51 +0900

    Update const.py

--- custom_components/kepco_realtime/const.py
+++ custom_components/kepco_realtime/const.py
@@ -1,10 +1,10 @@
 DOMAIN = "kepco_realtime"
 
 # API URLs
-BASE_URL = "https://pp.kepco.co.kr:8030"
-INTRO_URL = f"{BASE_URL}/intro.do"
-LOGIN_URL = f"{BASE_URL}/login"
-RECENT_USAGE_URL = f"{BASE_URL}/low/main/getRM0201.do"
+BASE_URL = "https://pp.kepco.co.kr"
+INTRO_URL = f"{BASE_URL}:8030/intro.do"
+LOGIN_URL = f"{BASE_URL}:8030/login"
+RECENT_USAGE_URL = f"{BASE_URL}/rm/getRM0201.do"
 
 # 기본 업데이트 주기 (분)
 DEFAULT_SCAN_INTERVAL = 30
