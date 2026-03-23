cdb45c18ab74f7abf57b7383baf95c814d12ac7d
Murian <y2k001hj@gmail.com>
2026-03-23 10:31:21 +0900

    Update const.py

--- custom_components/kepco_realtime/const.py
+++ custom_components/kepco_realtime/const.py
@@ -2,8 +2,8 @@ DOMAIN = "kepco_realtime"
 
 # API URLs
 BASE_URL = "https://pp.kepco.co.kr"
-INTRO_URL = f"{BASE_URL}:8030/intro.do"
-LOGIN_URL = f"{BASE_URL}:8030/login"
+INTRO_URL = f"{BASE_URL}/intro.do"
+LOGIN_URL = f"{BASE_URL}/login"
 RECENT_USAGE_URL = f"{BASE_URL}/rm/getRM0201.do"
 
 # 기본 업데이트 주기 (분)
